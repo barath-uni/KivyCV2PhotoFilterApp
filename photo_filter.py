d496a65c41bdb1759b370908ebf7b1f79ea0fd97
Baradwaj Varadharajan <baradwav@amazon.com>
2021-06-19 17:48:24 +0530

    Commit filter app

---
+++ photo_filter.py
@@ -0,0 +1,54 @@
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRaisedButton
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+import cv2
+import pytesseract
+from kivymd.uix.label import MDLabel
+
+
+class MainApp(MDApp):
+
+    def build(self):
+        layout = MDBoxLayout(orientation='vertical')
+        self.image = Image()
+        self.label = MDLabel()
+        layout.add_widget(self.image)
+        layout.add_widget(self.label)
+        self.save_img_button = MDRaisedButton(
+            text="CLICK HERE",
+            pos_hint={'center_x': .5, 'center_y': .5},
+            size_hint=(None, None))
+        self.save_img_button.bind(on_press=self.take_picture)
+        layout.add_widget(self.save_img_button)
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.load_video, 1.0/30.0)
+        return layout
+
+    def load_video(self, *args):
+        ret, frame = self.capture.read()
+        # Frame initialize
+        self.image_frame = frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        edge = cv2.Canny(gray, 10, 100)
+        buffer = cv2.flip(edge, 0).tostring()
+        texture = Texture.create(size=(edge.shape[1], edge.shape[0]), colorfmt='luminance')
+        texture.blit_buffer(buffer, colorfmt='luminance', bufferfmt='ubyte')
+        self.image.texture = texture
+
+    def take_picture(self, *args):
+        image_name = "picture_at_2_02.png"
+        img = cv2.cvtColor(self.image_frame, cv2.COLOR_BGR2GRAY)
+        img = cv2.GaussianBlur(img, (3, 3), 0)
+        img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+        text_data = pytesseract.image_to_string(img, lang='eng', config="--oem 3 --psm 6")
+        print(text_data)
+        self.label.text = text_data
+        cv2.imshow("cv2 final image", img)
+        cv2.imwrite(image_name, self.image_frame)
+
+
+if __name__ == '__main__':
+    MainApp().run()
